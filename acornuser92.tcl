cf7be862405c7e10cfe5ab422f68e977d43988b4
PM <dusthillresident@gmail.com>
2022-10-11 17:54:38 +0100

    Add files via upload

--- acornuser92.tcl
+++ acornuser92.tcl
@@ -13,6 +13,8 @@ tk appname "AcornUser August 92 water surface, original by Michel Grimminck"
 
 set wh 63 ;# width and height of the water surface
 set sc 8  ;# scale size used for drawing
+set xs 1
+set ys 1
 
 # initialise arrays
 for {set x 0} {$x<$wh} {incr x} {
@@ -45,31 +47,33 @@ proc process_frame {} {
 
 set height("32,32") -10
 
-pack [canvas .squares -width [expr {$wh*$sc}] -height [expr {$wh*$sc}]] -fill none -expand 0 
+#pack [canvas .squares -width [expr {$wh*$sc}] -height [expr {$wh*$sc}] -background black] -fill both -expand 1
+image create photo img
+image create photo displ
+pack [label .squares -image displ] -fill both -expand 1
+img put black -to 0 0 $wh $wh
 
 proc display_frame {} {
- foreach itm [.squares find all] {
-  .squares delete $itm
- }
- global wh height velocity cols
+ global wh height cols
  for {set x 1} {$x<$wh-1} {incr x} {
   for {set y 1} {$y<$wh-1} {incr y} { 
    set colour [expr { int($height("$x,$y")*4) }]
    if {$colour<-4} {set colour -4}
    if {$colour>4}  {set colour 4}
    set colour $cols($colour)
-   .squares create rectangle [expr {$x*$::sc}] [expr {$y*$::sc}] [expr {$x*$::sc+$::sc}] [expr {$y*$::sc+$::sc}] -fill $colour -outline $colour
+   img put $colour -to $x $y
   }
  }
+ displ copy img -zoom $::xs $::ys
 }
 
 set mouse_pressed 0
 
 proc clickaction {x y} {
- global height
- set xx [expr {int($x/$::sc)}]
+ global height xs ys
+ set xx [expr {int($x/$xs)}]
  set xx [expr { !($xx<1||$xx>=$::wh-1) ? $xx : -1 }]
- set yy [expr {int($y/$::sc)}]
+ set yy [expr {int($y/$ys)}]
  set yy [expr { !($yy<1||$yy>=$::wh-1) ? $yy : -1 }]
  set height("$xx,$yy") 10
 }
@@ -89,9 +93,19 @@ bind .squares <Motion> {
  }
 }
 
+bind . <Configure> {
+ displ blank
+ set ::xs [expr {[winfo width .squares]/$::wh}]
+ set ::ys [expr {[winfo height .squares]/$::wh}]
+ set ::xs [expr {$::xs<1 ? 1 : $::xs}]
+ set ::ys [expr {$::ys<1 ? 1 : $::ys}]
+}
+
 proc updater {} {
  process_frame
  display_frame
  after 6 updater
 }
 updater
+
+after 32 {wm geometry . 256x256}
